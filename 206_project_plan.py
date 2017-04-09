de9b8462d3ac89e05330edfaf4d7832b3b016ab2
durantkm <durantkm@umich.edu>
2017-04-09 02:45:35 -0400

    FP import statements setup commit

---
+++ 206_project_plan.py
@@ -0,0 +1,28 @@
+## Your name: Kyle Durant
+## The option you've chosen: Option 1
+
+# Put import statements you expect to need here!
+import requests
+import json
+import unittest
+import itertools
+import collections
+import sqlite3
+from bs4 import BeautifulSoup
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Write your test cases here.
+
+
+## Remember to invoke all your tests...
